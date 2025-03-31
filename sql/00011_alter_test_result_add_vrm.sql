02b27e96a4bbc378ec9a8f48f383632ff3439f1c
Ali Campbell <alistair.campbell@bjss.com>
2025-03-31 10:58:06 +0100

    Rename file, make column nullable

--- sql/00011_alter_test_result_add_vrm.sql
+++ sql/00011_alter_test_result_add_vrm.sql
@@ -1,3 +1,3 @@
 --liquibase formatted sql
 --changeset liquibase:modifyTable -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
-ALTER TABLE test_result ADD vrm VARCHAR(9) NOT NULL;
+ALTER TABLE test_result ADD vrm_trm VARCHAR(9) NULL;
