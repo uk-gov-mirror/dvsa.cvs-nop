f8dc77dab769677e0a240088e5d2ec722e4d5525
Jack Anderson <jack.anderson@bjss.com>
2025-04-01 12:07:26 +0100

    addColumn liquibase id

--- sql/00011_alter_test_result_add_vrm.sql
+++ sql/00011_alter_test_result_add_vrm.sql
@@ -1,3 +1,3 @@
 --liquibase formatted sql
---changeset liquibase:modifyTable -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
+--changeset liquibase:addColumn -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
 ALTER TABLE test_result ADD vrm_trm VARCHAR(9) NULL;
