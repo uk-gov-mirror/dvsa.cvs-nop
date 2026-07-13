7e1ccb076243ddeef086495c099ea3c224173243
Meys <140601718+meys-bjss@users.noreply.github.com>
2026-07-13 15:48:13 +0100

    Feature/cb2 21631 - Add fail reasons for defect media to NOP test result defects schema (#69)
    * added media columns to test result and defects
    
    * added media tables
    
    * in creasing varchar length for path and reason to 150 and 500
    
    * update media.'reason' to allow NULL
    
    * added UNIQUE INDEX to media tables
    
    * fix: updated file name
    
    * Feature/cb2 19647 - Add Vehicle Load Status to Test Result schema in NOP (#71)
    
    * adding vehilce load status fields to test-types
    
    * updated column type
    
    * added FOREIGN KEY  to the vehicle_load_status table definition
    
    * added UNIQUE INDEX to vehicle load status tables
    
    * updated table names
    
    * updated column names
    
    * fix: column name updated for load_status table
    
    * fix: column name updated for load_status table
    
    * fix: column name updated for load_status table
    
    * updated unique index members for load_status table
    
    * fix: rename file name
    
    * fix: rename file name

---
+++ sql/00013_create_media_tables.sql
@@ -0,0 +1,61 @@
+--liquibase formatted sql
+--changeset liquibase:add-media-tables -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
+
+CREATE TABLE IF NOT EXISTS `media_type`
+(
+    `id`          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `type`        VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`id`),
+
+    UNIQUE INDEX `idx_media_type_uq` (`type`)
+
+)
+    ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `defect_media`
+(
+    `id`             BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `test_defect_id` BIGINT UNSIGNED NOT NULL,
+    `path`           VARCHAR(150) NOT NULL,
+    `reason`         VARCHAR(500) NULL,
+    `media_type_id`  BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`test_defect_id`)
+        REFERENCES test_defect (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+
+    FOREIGN KEY (`media_type_id`)
+        REFERENCES media_type (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+
+    UNIQUE INDEX `idx_defect_media_uq` (`test_defect_id` ASC, `path` ASC, `reason` ASC, `media_type_id` ASC)
+    
+)
+    ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `test_result_media`
+(
+    `id`             BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `test_result_id` BIGINT UNSIGNED NOT NULL,
+    `path`           VARCHAR(150) NOT NULL,
+    `reason`         VARCHAR(500) NULL,
+    `media_type_id`  BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`test_result_id`)
+        REFERENCES test_result (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+
+    FOREIGN KEY (`media_type_id`)
+        REFERENCES media_type (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    
+    UNIQUE INDEX `idx_test_result_media_uq` (`test_result_id` ASC, `path` ASC, `reason` ASC, `media_type_id` ASC)
+
+)
+    ENGINE = InnoDB;
